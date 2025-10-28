ad6624a3374fd1f40f333d50fbd85f664809ee59
prylatief <prilatief@gmail.com>
2025-10-28 22:18:15 +0700

    Update Vite config to set chunk size warning limit
    Added build configuration to increase chunk size warning limit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // ... konfigurasi lainnya
+  build: {
+    // Properti ini yang akan menghilangkan peringatan chunk size
+    chunkSizeWarningLimit: 2000 // Mengatur batas peringatan menjadi 2000 kB (2 MB)
+  }
 })
